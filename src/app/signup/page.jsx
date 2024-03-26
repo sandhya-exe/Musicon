b1e208303709d49fee6dc56185cb7a1ba50fded3
Sandhya <85215513+sandhya-exe@users.noreply.github.com>
2024-03-26 23:13:55 +0530

    Update page.jsx

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -37,7 +37,7 @@ const page = () => {
           userName: formData.userName,
           email: formData.email,
           password: formData.password,
-          imageUrl: `https://api.dicebear.com/8.x/thumbs/svg`,
+          imageUrl: `https://api.dicebear.com/6.x/thumbs/svg?seed=${formData.userName}`,
         }),
       });
       const data = await res.json();
